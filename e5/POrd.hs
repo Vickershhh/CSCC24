b013d562088ec405a090cb52413b339b51704988
Vickershhh <haitao.zhu@mail.utoronto.ca>
2017-03-29 14:19:06 -0400

    Add files via upload

--- e5/POrd.hs
+++ e5/POrd.hs
@@ -2,20 +2,12 @@ module POrd where
 
 import Set
 
-class POrd a where 
-	pcompare :: a -> a -> Bool
+class POrd a where
+    lt, gt, lte, gte, inc :: a -> a -> Bool
+    pcompare :: a -> a -> POrdering
 
-	lt :: a -> a -> Bool
-    lt x y = pcompare x y == PLT
-
-    gt :: a -> a -> Bool
     gt x y = pcompare x y== PGT
-
-    lte :: a -> a -> Bool
-    lte x y = (pcompare x y == PLT) | (pcompare x y == PEQ)
-
-    gte :: a -> a -> Bool
-    gte x y = (pcompare x y == PGT) | (pcompare x y == PEQ)
-
-    inc :: a -> a -> Bool
+    lt x y = pcompare x y == PLT
+    lte x y = (pcompare x y == PLT) || (pcompare x y == PEQ)
+    gte x y = (pcompare x y == PGT) || (pcompare x y == PEQ)
     inc x y = pcompare x y == PIN
